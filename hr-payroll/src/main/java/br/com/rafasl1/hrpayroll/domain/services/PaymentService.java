41207361536aa2b6eb5733dc991d6e131bb399b5
Rafael Silva de Lima <rafaellima1100@gmail.com>
2023-08-22 23:36:08 -0300

    feat: change rest template call to feign client call

--- hr-payroll/src/main/java/br/com/rafasl1/hrpayroll/domain/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/rafasl1/hrpayroll/domain/services/PaymentService.java
@@ -3,6 +3,7 @@ package br.com.rafasl1.hrpayroll.domain.services;
 import br.com.rafasl1.hrpayroll.domain.entities.Payment;
 import br.com.rafasl1.hrpayroll.domain.entities.Worker;
 import br.com.rafasl1.hrpayroll.domain.entities.WorkerClient;
+import br.com.rafasl1.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,15 +11,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class PaymentService {
 
-    private WorkerClient workerClient;
-
     @Autowired
-    public PaymentService(WorkerClient workerClient) {
-        this.workerClient = workerClient;
-    }
+    private WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(long workerId, int days) {
-        Worker worker = workerClient.getWorker(workerId, days);
+        Worker worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 
