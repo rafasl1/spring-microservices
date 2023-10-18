781cf6338dbcc1e5e9eacd46c7b2d0c0f42fe02a
Rafael Silva de Lima <rafaellima1100@gmail.com>
2023-10-18 20:43:55 -0300

    feat: create endpoint to get user by email

--- hr-user/src/main/java/br/com/rafasl1/hruser/resources/UserResource.java
+++ hr-user/src/main/java/br/com/rafasl1/hruser/resources/UserResource.java
@@ -4,10 +4,7 @@ import br.com.rafasl1.hruser.entities.User;
 import br.com.rafasl1.hruser.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -19,6 +16,12 @@ public class UserResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Long id) {
         User user = userRepository.findById(id).get();
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping(value = "/search")
+    public ResponseEntity<User> findByEmail(@RequestParam String email) {
+        User user = userRepository.findByEmail(email);
         return ResponseEntity.ok(user);
     }
 
